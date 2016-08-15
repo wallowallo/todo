1c2a6201541b2b0495326cc01f4e3bb47cfcf25d
Krister <krister.torsvik@gmail.com>
2016-08-15 20:56:39 +0200

    wip: todo

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function createTodo() {
   var newDiv = document.createElement("div");
     newDiv.className = "todo"
     function addText() {
-      document.getElementById("todo").innerHTML = document.querySelector("input[type=text]").value;
-    }
+      document.getElementById("todo").innerHTML = document.querySelectorAll("input[type=text]").value;
 
+    }
+    addText();
     // add the newly created element and its content into the DOM
     var currentDiv = document.getElementById("div");
     document.body.insertBefore(newDiv, currentDiv);
